190714385f3bc84a1441665fc2a0991e38fdea0b
semblant <robynhumphreys7_4@hotmail.com>
2025-04-09 22:52:06 -0600

    Seed rest of data

--- server/db/schema/create.sql
+++ server/db/schema/create.sql
@@ -9,17 +9,6 @@ CREATE TABLE Users (
   username VARCHAR(255) NOT NULL
 );
 
--- Create EXPENSES table with a foreign key to USERS
-CREATE TABLE Expenses (
-  expense_id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER NOT NULL,
-  name VARCHAR(100) NOT NULL,
-  amount NUMERIC(15, 2) NOT NULL,
-  expense_date DATE NOT NULL,
-  category VARCHAR(50),
-  FOREIGN KEY (user_id) REFERENCES Users(id)
-);
-
 -- Create INCOME table with a foreign key to USERS
 CREATE TABLE Income (
   income_id SERIAL PRIMARY KEY NOT NULL,
@@ -27,6 +16,17 @@ CREATE TABLE Income (
   amount NUMERIC(15, 2) NOT NULL,
   frequency VARCHAR(20) NOT NULL,
   last_payment_date DATE NOT NULL,
-  next_payment_date DATE NOT NULL,
   FOREIGN KEY (user_id) REFERENCES Users(id)
 );
+
+-- Create EXPENSES table with a foreign key to USERS
+CREATE TABLE Expenses (
+  expense_id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER NOT NULL,
+  -- name VARCHAR(100) NOT NULL,
+  amount NUMERIC(15, 2) NOT NULL,
+  expense_date DATE NOT NULL,
+  category VARCHAR(50),
+  FOREIGN KEY (user_id) REFERENCES Users(id)
+);
+
--- server/db/schema/seed.sql
+++ server/db/schema/seed.sql
@@ -1,12 +1,22 @@
-INSERT INTO USERS(ID, USERNAME)
+INSERT INTO Users(id, username)
 VALUES
   (1,'jdoe'),
   (2, 'awond'),
   (3, 'sitad'),
-  (4, 'matte'),
-  (5, 'anita'),
-  (6,  'lsouza'),
-  (7, 'josea'),
-  (8, 'jdwayne'),
-  (9, 'saeng'),
-  (10, 'santa');
+  (4, 'matte');
+
+INSERT INTO Income(user_id, amount, frequency, last_payment_date)
+VALUES
+  (1, 2000, 'semi-monthly', TO_DATE('15/02/2025', 'DD/MM/YYYY') ),
+  (3, 1800, 'semi-monthly', TO_DATE('01/03/2025', 'DD/MM/YYYY') ),
+  (4, 1500, 'semi-monthly', TO_DATE('15/03/2025', 'DD/MM/YYYY')),
+  (1, 1200, 'semi-monthly', TO_DATE('01/03/2025', 'DD/MM/YYYY'));
+
+
+
+INSERT INTO Expenses(user_id, amount, expense_date, category)
+VALUES
+  (2, -400, TO_DATE('10/02/2025', 'DD-MM-YYYY'), 'Car Insurance'),
+  (1, -1200,TO_DATE('01/03/2025', 'DD-MM-YYYY'), 'Rent'),
+  (4, -85, TO_DATE('20/03/2025', 'DD-MM-YYYY'), 'Phone Bill'),
+  (4,  -250, TO_DATE('25/03/2025', 'DD-MM-YYYY'), 'Groceries');
