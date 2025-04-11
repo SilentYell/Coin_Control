862a73a168f717c16b9140c44773c0376af6a99e
pheyboer <heyboer.peter@gmail.com>
2025-04-11 12:54:26 -0400

    installing cors and implementing routes in index.js to test routes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const cors = require('cors'); // install cors: npm install cors
 const app = express();
 const port = 3000;
+
+// import routes
+const expenseRoutes = require('./routes/expenseRoutes');
+const userRoutes = require('./routes/userRoutes');
+
+// middleware
+app.use(cors());
+app.use(express.json()); // allows parsing JSON request bodies
+
+// use routes
+app.use('/api', expenseRoutes);
+app.use('/api', userRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello from Coin Control backend!');
