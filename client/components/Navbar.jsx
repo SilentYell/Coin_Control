f64bc199543bba7304f616fc12bd4495101bb6f3
Olivia <olivialanderson1994@gmail.com>
2025-04-13 16:56:01 -0700

    adjusted styling for dashboard component to fill screen

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/Navbar.scss';
 
-const Navbar = () => {
+const Navbar = ({ user, handleLogin, handleLogout }) => {
+
   return (
     <nav className='navbar'>
       <div className='navbar-logo'>
         <span>Coin Control</span>
       </div>
-      <ul>
-        <li><a href="#">Expense History</a></li>
-        <li><a href="#">Add Expense</a></li>
-        <li><a href="#">Income History</a></li>
-        <li><a href="#">Add Income</a></li>
-        <li><a href="#">Trophy Case</a></li>
+
+      <ul className='navbar-links'>
+        {user ? (
+          <>
+            <li><a href="#">Expense History</a></li>
+            <li><a href="#">Add Expense</a></li>
+            <li><a href="#">Income History</a></li>
+            <li><a href="#">Add Income</a></li>
+            <li><a href="#">Trophy Case</a></li>
+          </>
+        ) : (
+          <></>
+        )}
       </ul>
-      <button className='logout-btn'>Logout</button>
+
+      <div className='navbar-user'>
+        {!user ? (
+          // If no user, show login button
+          <button className='login-btn' onClick={handleLogin}>Login</button>
+        ) : (
+          <div className='user-info'>
+            <p>Welcome, {user.username}</p>
+            <p>Balance: ${user.current_balance.toFixed(2)}</p>
+          <button className='logout-btn' onClick={handleLogout}>Logout</button>
+          </div>
+        )}
+      </div>
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar;
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -34,41 +34,24 @@ function App() {
     });
   };
 
+  const handleLogout = () => {
+    setUser(null);
+  }
+
   // Need navigation bar to see expense and income lists
 
   return (
     <div className="App" style={{ textAlign: 'center', marginTop: '50px' }}>
-      {!user ? (
-        <button
-          onClick={handleLogin}
-          style={{
-            padding: '10px 20px',
-            fontSize: '16px',
-            backgroundColor: '#4CAF50',
-            color: 'white',
-            border: 'none',
-            borderRadius: '5px',
-            cursor: 'pointer',
-          }}
-        >
-          Login
-        </button>
-      ) : (
-        <div>
-          <h1>Welcome, {user.username}!</h1>
-          <p>Current Balance: ${user.current_balance.toFixed(2)}</p>
+          
+          {/* Adding Navbar Component */}
+          <Navbar user={user} handleLogin={handleLogin} handleLogout={handleLogout} />
           {/* Adding Dashboard Component */}
-          <Navbar />
           <Dashboard />
-
-
           {/* Adding Expense Form Component */}
           {/* <AddExpenseForm /> */}
 
           {/* Adding Expenses List Component */}
           {/* <ExpensesList /> */}
-        </div>
-      )}
 
       {/* Conditional Rendering for temporary Income list and form buttons */}
       {user && <button
