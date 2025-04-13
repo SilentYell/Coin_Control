da3a854258b1b8f449d191c4f0795e884b3be6b3
semblant <robynhumphreys7_4@hotmail.com>
2025-04-13 15:31:11 -0600

    Add post method to /api/income and add functionality to handleSubmit

--- server/routes/income.js
+++ server/routes/income.js
@@ -18,5 +18,30 @@ module.exports = db => {
     });
   });
 
+
+  router.post("/income", (req, res) => {
+    const { amount, last_payment_date, frequency } = req.body;
+    const user_id = 1 // should come from req.body, will change to handle dynamic user ID later
+
+
+    const query = `
+    INSERT INTO income (user_id, amount, last_payment_date, frequency)
+    VALUES ($1, $2, $3, $4)
+    RETURNING *;
+    `;
+
+    const queryParams = [user_id, amount, last_payment_date, frequency];
+
+    db.query(query, queryParams)
+    .then(result => {
+      res.status(201).json(result.rows[0]);
+    })
+    .catch(err => {
+      console.error('Error inserting income', err);
+      res.status(500).json({error: 'Internal server error'});
+    });
+  });
+
+
   return router;
 };
