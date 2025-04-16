4dcbc83977f1a85b61d8c066cce72ebfe6cbff42
Nik Hixson <nikilobe@gmail.com>
2025-04-16 11:26:52 -0700

    add animation effects to Card component and include animejs dependency

--- client/components/Card.jsx
+++ client/components/Card.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import anime from 'animejs/lib/anime.es.js';
+import React, { useEffect, useRef } from 'react';
 import '../styles/Card.scss';
 
 function Card({ title, value, description }) {
+  const cardRef = useRef(null);
+
+  useEffect(() => {
+    if (cardRef.current) {
+      anime({
+        targets: cardRef.current,
+        opacity: [0, 1],
+        translateY: [40, 0],
+        duration: 800,
+        easing: 'easeOutExpo',
+      });
+    }
+  }, []);
+
   return (
-    <div className="card">
+    <div className="card" ref={cardRef}>
       <div className="card-title">{title}</div>
       <div className="card-value">{value}</div>
       <div className="card-description">{description}</div>
