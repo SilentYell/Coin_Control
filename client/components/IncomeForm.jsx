07bae9a8a88a8431c0657f118e1c41f0b480288c
semblant <robynhumphreys7_4@hotmail.com>
2025-04-19 16:39:17 -0600

    Fix comments

--- client/components/IncomeForm.jsx
+++ client/components/IncomeForm.jsx
@@ -14,7 +14,7 @@ const IncomeForm = ({ editingIncome, onSubmitSuccess }) => {
       setFormData(initializeIncomeFormData(editingIncome));
   }, [editingIncome?.income_id]);
 
-  // common income categories
+  // Common income frequencies
   const frequencies = [
     'One-Time',
     'Daily',
