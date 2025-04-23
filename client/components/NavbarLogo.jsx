e24be3d4a39ca470a959c601339bf34dc5b44d8e
pheyboer <heyboer.peter@gmail.com>
2025-04-23 14:54:45 -0400

    implementing navbarlogo sizes

--- client/components/NavbarLogo.jsx
+++ client/components/NavbarLogo.jsx
@@ -1,3 +1,11 @@
 import React, { useEffect } from 'react';
 import anime from 'animejs/lib/anime.es.js';
 
+const NavbarLogo = ({ size = 'small' }) => {
+  // different sizes for different contexts
+  const sizes = {
+    small: { width: '40px', height: '40px' },
+    medium: { width: '60px', height: '60px' },
+    large: { width: '100px', height: '100px' },
+  };
+};
