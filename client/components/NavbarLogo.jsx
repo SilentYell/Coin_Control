57d71ebbe5b559b1a333ab891ee5d94628dcd15b
pheyboer <heyboer.peter@gmail.com>
2025-04-23 15:12:37 -0400

    editing nav bar styles and logos

--- client/components/NavbarLogo.jsx
+++ client/components/NavbarLogo.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react';
-import anime from 'animejs/lib/anime.es.js';
+import React from 'react';
 
 const NavbarLogo = ({ size = 'small' }) => {
   // different sizes for different contexts
