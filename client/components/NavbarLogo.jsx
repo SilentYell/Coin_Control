038e2ee873fe670ecc5c11b8e989a3247180f175
pheyboer <heyboer.peter@gmail.com>
2025-04-23 15:06:36 -0400

    animations not need for logo in nav bar

--- client/components/NavbarLogo.jsx
+++ client/components/NavbarLogo.jsx
@@ -9,33 +9,6 @@ const NavbarLogo = ({ size = 'small' }) => {
     large: { width: '100px', height: '100px' },
   };
 
-  useEffect(() => {
-    // coin animation
-    const outlines = document.querySelectorAll('.nav-coin-outline');
-    outlines.forEach((outline) => {
-      const length = outline.getTotalLength();
-      outline.style.strokeDasharray = length;
-      outline.style.strokeDashoffset = length;
-      anime({
-        targets: outline,
-        strokeDashoffset: [length, 0],
-        duration: 2000,
-        easing: 'easeInOutSine',
-        delay: 0
-      });
-    });
-
-    // letter animation
-    const cLetters = document.querySelectorAll('.nav-coin-c');
-    anime({
-      targets: cLetters,
-      opacity: [0, 1],
-      duration: 1200,
-      easing: 'easeInOutSine',
-      delay: 500
-    });
-  }, []);
-
   return (
     <div style={sizes[size]}>
       <svg
