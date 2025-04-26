b7fc1ea36395c7b5d0e47bdd0e9bae7b0f29fe9e
semblant <robynhumphreys7_4@hotmail.com>
2025-04-26 16:18:46 -0600

    Render Trophies inside TrophyCase Modal

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -7,6 +7,7 @@ import AddExpenseForm from './AddExpenseForm';
 import ExpensesList from './ExpensesList';
 import IncomeList from './IncomeList';
 import TrophyCase from './TrophyCase';
+import Trophies from './Trophies'
 import NavbarLogo from './NavbarLogo'
 import { FaBars, FaTimes } from 'react-icons/fa';
 import AllTransactions from './AllTransactions';
@@ -435,8 +436,12 @@ const Navbar = (
         </Modal>
       )}
       {showTrophyCaseModal && (
-        <Modal isOpen={showTrophyCaseModal} onClose={() => setShowTrophyCaseModal(false)}>
+        <Modal
+          className="trophy-case-modal"
+          isOpen={showTrophyCaseModal}
+          onClose={() => setShowTrophyCaseModal(false)}>
           <TrophyCase userId={user.user_id} trophiesList={trophiesList} setTrophiesList={setTrophiesList}/>
+          <Trophies trophiesList={trophiesList} setTrophiesList={setTrophiesList} />
         </Modal>
       )}
     </>
